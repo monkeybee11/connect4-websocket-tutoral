bceb48b433d97f313a77bff1c9391fa8fa6b2976
monkeybee11 <monkeybee1@googlemail.com>
2022-07-06 18:35:22 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import asyncio
 import json
+import os
 import secrets
+import signal
 
 import websockets
 
@@ -180,8 +182,14 @@ async def handler(websocket):
 
 
 async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        await stop
 
 
 if __name__ == "__main__":
